5a96fdaba81e90eb3a2ffbe542b223f1fe01a12c
Edward Thomson <ethomson@edwardthomson.com>
2024-12-31 12:02:50 +0000

    cmake: standardize leak check option
    The `GIT_WIN32_LEAKCHECK` option is a debugging option, so it should be
    `GIT_DEBUG_LEAKCHECK_WIN32`

--- cmake/AddClarTest.cmake
+++ cmake/AddClarTest.cmake
@@ -1,5 +1,5 @@
 function(ADD_CLAR_TEST project name)
-        if(NOT DEBUG_LEAK_CHECKER STREQUAL "OFF")
+        if(NOT DEBUG_LEAK_CHECKER STREQUAL "OFF" AND NOT DEBUG_LEAKCHECKER STREQUAL "win32")
                 add_test(${name} "${PROJECT_SOURCE_DIR}/script/${DEBUG_LEAK_CHECKER}.sh" "${PROJECT_BINARY_DIR}/${project}" ${ARGN})
         else()
                 add_test(${name} "${PROJECT_BINARY_DIR}/${project}" ${ARGN})
